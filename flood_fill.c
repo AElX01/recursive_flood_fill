a34a5865e8fd9d2c3d011843d093daa17aa94d38
0xNcat <daniel.juarez@iteso.mx>
2024-10-28 19:00:58 -0600

    first layout

---
+++ flood_fill.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+#include "queue.h"
+
+#define ROWS 20
+#define COLS 30
+
+
+void flood_fill();
+void flood_fill_stack();
+void flood_fill_queue();
+
+int main()
+{
+    return 0;
+}
